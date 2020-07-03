b4dcef817763f0aae63acec6fd8503a369da41c7
Magnus Vestergaard <Magnusvestergaard@live.dk>
2020-07-03 08:56:19 +0200

    Update

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 //Test
-const fetch = require("fetch");
+const   fetch = require("fetch")
+        ,express = require("express")
+        ,app = express()
+        ,port = 8008
+
+app.get('/', (req, res) => res.send('Hello World'));
+
+app.listen(port, () => console.log(`EFTItems running at http://localhost:${port}`));
